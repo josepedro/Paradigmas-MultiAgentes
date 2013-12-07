20742aab20b022286db6de7efc8c1987621b75e4
thiago de souza <thiagitosouza@hotmail.com>
2013-12-07 12:45:20 -0200

    Adicionando ENUM de cartas

---
+++ Truco/src/regrasTruco/Cartas.java
@@ -0,0 +1,16 @@
+package regrasTruco;
+
+
+	public enum Cartas {
+		DOIS(6,4), TRES(7,4), QUATRO_PAUS(11,1), SETE_COPAS(10,1), SETE_OUROS(8,1),
+		ESPADILHA(9,1), VALETE(2,4), DAMA(1,4), REI(4,4), AS(5,3);
+		
+		public final int peso;
+		public final int quantidade;
+		
+		Cartas(int peso , int quantidade){
+			this.peso=peso;
+			this.quantidade = quantidade;
+		}
+	}
+
