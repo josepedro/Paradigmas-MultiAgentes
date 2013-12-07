9f5bde71102387564b53c45d5ed16236a0ed78f3
thiago de souza <thiagitosouza@hotmail.com>
2013-12-07 12:45:00 -0200

    adicionando baralho

---
+++ Truco/src/regrasTruco/Baralho.java
@@ -0,0 +1,22 @@
+package regrasTruco;
+
+import java.util.ArrayList;
+
+public class Baralho {
+
+	void adicionarCartas(ArrayList<Cartas> baralho) {
+
+		baralho.add(Cartas.AS);
+		baralho.add(Cartas.ESPADILHA);
+		baralho.add(Cartas.DOIS);
+		baralho.add(Cartas.TRES);
+		baralho.add(Cartas.QUATRO_PAUS);
+		baralho.add(Cartas.SETE_COPAS);
+		baralho.add(Cartas.SETE_OUROS);
+		baralho.add(Cartas.VALETE);
+		baralho.add(Cartas.DAMA);
+		baralho.add(Cartas.REI);
+
+	}
+
+}
