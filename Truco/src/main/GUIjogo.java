3161db039b73200bb98611b77b638bdff9bec42e
Gepeto <1jpsneto@gmail.com>
2013-12-11 21:17:03 -0200

    Agente Novato criado com sucesso e com comportamentos.

--- Truco/src/main/GUIjogo.java
+++ Truco/src/main/GUIjogo.java
@@ -17,8 +17,8 @@ public class GUIjogo extends JFrame {
 	JPanel pnl_feedback = new JPanel();
 	JLabel feedback = new JLabel();
 	GridLayout gridLayout = new GridLayout();
-	
-	
+	JButton botao_matar_agente = new JButton();
+
 	protected JogadorDeInicio jogadorDeInicio;
 
 	public GUIjogo(JogadorDeInicio jogadorDeInicio) {
@@ -39,80 +39,53 @@ public class GUIjogo extends JFrame {
 		botao_joga_carta.setText("Jogue Carta");
 		botao_joga_carta.addActionListener(new java.awt.event.ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				btn_Exit_actionPerformed(e);
+				botaoJogaCartaListener(e);
 			}
 		});
 		botao_aceita_truco.setText("Aceita Truco?");
 		botao_aceita_truco
 				.addActionListener(new java.awt.event.ActionListener() {
 					public void actionPerformed(ActionEvent e) {
-						btn_stop_actionPerformed(e);
+						botaoAceitaTrucoListener(e);
 					}
 				});
 		botao_pede_truco.setText("Pede Truco?");
 		botao_pede_truco.addActionListener(new java.awt.event.ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				btn_start_actionPerformed(e);
+				botaoPedeTrucoListener(e);
 			}
 		});
+		botao_matar_agente.setText("Matar jogador");
+		botao_matar_agente
+				.addActionListener(new java.awt.event.ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						botaoMataAgenteListener(e);
+					}
+				});
 		this.setTitle("Truco");
-		this.addWindowListener(new java.awt.event.WindowAdapter() {
-			public void windowClosing(WindowEvent e) {
-				this_windowClosing(e);
-			}
-		});
 		this.getContentPane().add(pnl_main, BorderLayout.CENTER);
 		pnl_main.add(botao_joga_carta, null);
 		pnl_main.add(botao_pede_truco, null);
 		pnl_main.add(botao_aceita_truco, null);
+		pnl_main.add(botao_matar_agente,null);
 
 	}
 
-	/**
-	 * When the user clicks on start, notify the host to begin the party.
-	 */
-	void btn_start_actionPerformed(ActionEvent e) {
-		// enableControls( true );
-		//
-		// // add a behaviour to the host to start the conversation going
-		// m_owner.addBehaviour( new OneShotBehaviour() {
-		// public void action() {
-		// ((HostAgent) myAgent).inviteGuests( slide_numGuests.getValue() );
-		// }
-		// } );
+	void botaoPedeTrucoListener(ActionEvent e) {
+		jogadorDeInicio.pedeTruco();
 	}
 
-	/**
-	 * When the user clicks on stop, tell the host to stop the party.
-	 */
-	void btn_stop_actionPerformed(ActionEvent e) {
-		// enableControls( false );
-		//
-		// // add a behaviour to the host to end the party
-		// m_owner.addBehaviour(() new OneShotBehaviour() {
-		// public void action() {
-		// ((HostAgent) myAgent).endParty();
-		// }
-		// } );
+	void botaoAceitaTrucoListener(ActionEvent e) {
+		jogadorDeInicio.aceitaTruco();
 	}
 
-	void enableControls(boolean starting) {
-		botao_pede_truco.setEnabled(!starting);
-		botao_aceita_truco.setEnabled(starting);
-		botao_joga_carta.setEnabled(!starting);
+	void botaoJogaCartaListener(ActionEvent e) {
+		jogadorDeInicio.jogaCarta();
 	}
-
-	void btn_Exit_actionPerformed(ActionEvent e) {
-		// m_owner.addBehaviour( new OneShotBehaviour() {
-		// public void action() {
-		// ((HostAgent) myAgent).terminateHost();
-		// }
-		// } );
-	}
-
-	void this_windowClosing(WindowEvent e) {
-		// simulate the user having clicked exit
-		btn_Exit_actionPerformed(null);
+	
+	void botaoMataAgenteListener(ActionEvent e){
+		jogadorDeInicio.doDelete();
+		System.out.println("Jogador "+jogadorDeInicio.getLocalName()+" morto!");
 	}
 
 }
--- Truco/src/main/JogadorDeInicio.java
+++ Truco/src/main/JogadorDeInicio.java
@@ -1,13 +1,11 @@
 package main;
 
 import jade.core.Agent;
-
-import jade.core.Agent;
+import jade.core.behaviours.SimpleBehaviour;
 import jade.domain.DFService;
 import jade.domain.FIPAException;
 import jade.domain.FIPAAgentManagement.DFAgentDescription;
-
-import javax.swing.JFrame;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
 
 /**
  * Implementando o jogador novato
@@ -16,32 +14,120 @@ public class JogadorDeInicio extends Agent {
 
 	protected GUIjogo m_frame = null;
 
-	protected void setup() {
-		System.out.println("O jogador "+getLocalName()+" está querendo jogar!");
-		
-		//Adicionando descricao ao agente jogador novato
-		DFAgentDescription descricaoAgente = new DFAgentDescription();
-		descricaoAgente.setName( getAID() );
-        try {
-			DFService.register( this, descricaoAgente);
-		} catch (FIPAException e) {
-			e.printStackTrace();
-		}
-        
-        //Iniciando a GUI de desenvolvimento do agente jogador
-        inciandoGUI();
-		
+	// Inserindo comportamento de jogar carta
+	class ComportamentoJogarCartas extends SimpleBehaviour {
 
-		// Make this agent terminate
-		doDelete();
+		// Instancia do comportamento
+		public ComportamentoJogarCartas(Agent agente) {
+			super(agente);
+		}
+
+		@Override
+		public void action() {
+			System.out.println("Eu " + myAgent.getLocalName()
+					+ " acabei de jogar uma carta.");
+		}
+
+		@Override
+		public boolean done() {
+			// O comportamento so sera executado uma vez
+			return true;
+		}
+
+	}
+
+	// Inserindo comportamento de pedir truco
+	class ComportamentoPedirTruco extends SimpleBehaviour {
+
+		// Instancia do comportamento
+		public ComportamentoPedirTruco(Agent agente) {
+			super(agente);
+		}
+
+		@Override
+		public void action() {
+			System.out.println("Eu " + myAgent.getLocalName()
+					+ " não quero pedir truco nem a pau.");
+		}
+
+		@Override
+		public boolean done() {
+			// O comportamento so sera executado uma vez
+			return true;
+		}
+
+	}
+	
+	// Inserindo comportamento de aceitar truco
+		class ComportamentoAceitarTruco extends SimpleBehaviour {
+
+			// Instancia do comportamento
+			public ComportamentoAceitarTruco(Agent agente) {
+				super(agente);
+			}
+
+			@Override
+			public void action() {
+				System.out.println("Eu " + myAgent.getLocalName()
+						+ " aceito truco na hora.");
+			}
+
+			@Override
+			public boolean done() {
+				// O comportamento so sera executado uma vez
+				return true;
+			}
+
+		}
+
+
+	protected void setup() {
+
+		System.out.println("O jogador " + getLocalName()
+				+ " está querendo jogar!");
+
+		// Adicionando descricao ao agente jogador novato
+		// DFAgentDescription descricaoAgente = new DFAgentDescription();
+		// descricaoAgente.setName(getAID());
+		// ServiceDescription servicoDescricao = new ServiceDescription();
+		// servicoDescricao
+		// .setType("Jogador Novato: joga cartas aleatorias; nunca pede truco; sempre aceita truco");
+		// servicoDescricao.setName("JADE-Novato");
+		// descricaoAgente.addServices(servicoDescricao);
+		// try {
+		// DFService.register(this, descricaoAgente);
+		// } catch (FIPAException e) {
+		// e.printStackTrace();
+		// }
+
+		//
+
+		// Iniciando a GUI de desenvolvimento do agente jogador
+		inciandoGUI();
+
 	}
 
 	private void inciandoGUI() {
 		m_frame = new GUIjogo(this);
+		m_frame.setSize(800, 200);
+		m_frame.setLocation(400, 400);
+		m_frame.setVisible(true);
+		m_frame.validate();
+	}
 
-        m_frame.setSize( 400, 200 );
-        m_frame.setLocation( 400, 400 );
-        m_frame.setVisible( true );
-        m_frame.validate();
+	public void jogaCarta() {
+		ComportamentoJogarCartas jogarCartas = new ComportamentoJogarCartas(
+				this);
+		addBehaviour(jogarCartas);
+	}
+
+	public void pedeTruco() {
+		ComportamentoPedirTruco pedirTruco = new ComportamentoPedirTruco(this);
+		addBehaviour(pedirTruco);
+	}
+	
+	public void aceitaTruco() {
+		ComportamentoAceitarTruco aceitarTruco = new ComportamentoAceitarTruco(this);
+		addBehaviour(aceitarTruco);
 	}
 }
